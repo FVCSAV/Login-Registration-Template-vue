f167fa2477c58a3a66f3ad749ea10ed502f7da2d
”FVCSAV“ <fvcsav@outlook.com>
2023-04-12 08:57:17 +0800

    创建项目

---
+++ qkqianhou-backend/src/main/java/com/deng/qkqianhoubackend/QkqianhouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deng.qkqianhoubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QkqianhouBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QkqianhouBackendApplication.class, args);
+	}
+
+}
